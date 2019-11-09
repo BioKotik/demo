7602ff2ca44042ed2a045bb9e029e68cf673ef08
Maxim Belov <belovmd@gmail.com>
2019-11-09 12:20:15 +0300

    commit

--- feed/urls.py
+++ feed/urls.py
@@ -5,4 +5,8 @@ app_name = 'feed'
 
 urlpatterns = [
     path('', views.all_records, name='list_view'),
+    path('<int:yy>/<int:mm>/<int:dd>/<slug:slug>',
+         views.detailed_view,
+         name='detailed_view'),
+    path('<int:record_id>/send/', views.send_email, name='send_email'),
 ]
